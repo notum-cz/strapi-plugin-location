0202ca98911df294b8d88ab8248ec29d9d6539fd
Ondrej Mikulcik <ondrej.mikulcik@notum.cz>
2023-08-14 12:42:00 +0200

    fix: deleted values for capitalized columns on startup

--- server/bootstrap.ts
+++ server/bootstrap.ts
@@ -21,7 +21,6 @@ export default async ({ strapi }: { strapi: Strapi }) => {
       const locationFields = strapi.services[
         locaitonServiceUid
       ].getLocationFields(model.attributes);
-
       await Promise.all(
         locationFields.map(async (locationField) => {
           const hasColumn = await db.schema.hasColumn(
@@ -38,11 +37,12 @@ export default async ({ strapi }: { strapi: Strapi }) => {
             _.snakeCase(locationField),
             "id"
           );
+          console.log(location, tableName, locationField);
           await Promise.all(
             location.map(async (entry) => {
-              const json = entry[locationField];
+              const json = entry[_.snakeCase(locationField)];
               if (!json?.lng || !json?.lat) return;
-
+              console.log(locationField);
               await db.raw(`
                 UPDATE ${tableName}
                 SET ${locationField}_geom = ST_SetSRID(ST_MakePoint(${json.lng}, ${json.lat}), 4326)
